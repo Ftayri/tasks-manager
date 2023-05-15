e4387af3e239e14ae738df02650ede4d211fe5bc
Iyed <75183184+Ftayri@users.noreply.github.com>
2023-05-15 23:31:56 +0100

    Login and register components. WIP: handle successful logins and registers

--- frontend/tasks-manager/src/app/login/login.component.ts
+++ frontend/tasks-manager/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -6,6 +8,31 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) {}
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+  login() {
+    console.log('Login form value:', this.loginForm.value);
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      this.authService.doEmailPasswordLogin(email, password)
+        .then((result: any) => {
+          // Login successful, handle success
+          console.log('Login successful:', result);
+          // Redirect to dashboard, clear form, etc.
+        })
+        .catch((error: any) => {
+          // Login failed, handle error
+          console.error('Login error:', error);
+          // Display error message, clear password, etc.
+        });
+    }
+  }
 
 }
