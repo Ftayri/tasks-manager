645ce468ee2ef61aa01aac629eaf2eda5179e072
Iyed <75183184+Ftayri@users.noreply.github.com>
2023-05-15 19:03:06 +0100

    Initialized authentication

--- frontend/tasks-manager/src/app/app.module.ts
+++ frontend/tasks-manager/src/app/app.module.ts
@@ -4,15 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { LoginComponent } from './login/login.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { RegistrationComponent } from './registration/registration.component';
+import {MatCardModule} from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FirebaseModule } from './Firebase.module';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    FirebaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
