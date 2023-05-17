7c18653a058bff3cbdea67ee66c9c222458610c9
Iyed <75183184+Ftayri@users.noreply.github.com>
2023-05-17 16:10:49 +0100

    WIP: Task edit and TaskService

--- frontend/src/app/modal-view/modal-view.component.ts
+++ frontend/src/app/modal-view/modal-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ToDoList } from 'app/models/to-do-list';
 
@@ -8,14 +9,34 @@ import { ToDoList } from 'app/models/to-do-list';
   styleUrls: ['./modal-view.component.scss']
 })
 export class ModalViewComponent implements OnInit {
-
   toDoList: ToDoList;
-  constructor(private dialogRef: MatDialogRef<ModalViewComponent>, @Inject(MAT_DIALOG_DATA) data:any) {
+  editIndex: number = -1;
+  constructor(private dialogRef: MatDialogRef<ModalViewComponent>, @Inject(MAT_DIALOG_DATA) data: any) {
     this.toDoList = data;
   }
-
+  taskForms: FormGroup[] = [];
+  initForms() {
+    this.toDoList.tasks.forEach((row) => {
+      const formGroup = new FormGroup({
+        title: new FormControl(row.title),
+        priority: new FormControl(row.priority),
+        status: new FormControl(row.status)
+      });
+      this.taskForms.push(formGroup);
+    });
+  }
+  editMode(index: number) {
+    this.editIndex = index;
+    console.log("edit mode")
+  }
+  cancelEdit(id: number) {
+      this.editIndex = -1;
+  }
+  editTask(id: String, index: number) {
+    console.log(this.taskForms[index].value);
+  }
   ngOnInit(): void {
-    console.log(this.toDoList);
+    this.initForms();
   }
 
 }
